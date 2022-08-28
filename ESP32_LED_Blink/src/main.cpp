fd02cc237c605a58232077ef05f46c46f1f33532
Pedro Odillon <pedroodillon@alu.ufc.br>
2022-08-28 19:11:47 -0300

    CHANGE

--- ESP32_LED_Blink/src/main.cpp
+++ ESP32_LED_Blink/src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
+#define LED 2   // PINO PARA O LED DA PLACA 
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  Serial.println("LED IS ON");
+  delay(200);
+
+  digitalWrite(LED, LOW);
+  Serial.println("LED IS OFF");
+  delay(200);
 }
